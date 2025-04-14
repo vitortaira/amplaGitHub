d0ae4c8b74800789afcb6001e83c34682906d932
vitortaira <vitortaira@gmail.com>
2025-04-14 12:07:49 -0300

    Reestruturação das funções de extração dos dados da CEF
    1) extrair_dados_arquivo_extrato_cef() foi renomeada para dados_cef_extrato().
    2) dados_cef_extratos() foi criada.

---
+++ dados/funcoes/dados_cef_extratos.r
@@ -0,0 +1,33 @@
+dados_cef_extratos <-
+  function(f_caminho.pasta.extratos_c =
+             here("Relatórios - Documentos", "Relatorios - Extratos")) {
+    # Consolida os dados dos extratos da CEF na pasta "Relatorios - Extratos"
+    caminhos.extratos.cef_c <-
+      list.files(
+        f_caminho.pasta.extratos_c,
+        full.names = TRUE, recursive = T
+      ) %>%
+      keep(
+        ~ str_ends(.x, ".pdf") &
+          str_detect(.x, "2429|2419|2245") &
+          !str_detect(.x, "(?i)fundo")
+      )
+    extratos_l <- list()
+    extratos_t <- data.frame()
+    for (
+      i_caminho.extrato.cef_c in caminhos.extratos.cef_c
+    ) {
+      extratos_l[[i_caminho.extrato.cef_c]] <-
+        dados_cef_extrato(i_caminho.extrato.cef_c)
+      extratos_t <-
+        bind_rows(extratos_t, extratos_l[[i_caminho.extrato.cef_c]])
+    }
+    extratos_t %<>%
+      mutate(
+        Contrato_6 =
+          Documento %>% str_pad(width = 6, side = "left", pad = "0")
+      ) %>%
+      as_tibble()
+    return(extratos_t)
+  }
+View(dados_cef_extratos())
