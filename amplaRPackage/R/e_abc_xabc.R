f9bf2f9dd97e7326f2a2b22b6ecea1fd8b398e46
vitortaira <vitortaira@gmail.com>
2025-06-18 16:58:28 -0300

    Refactor e_abc_xabc function: reorganize data extraction logic and update variable names for clarity; enhance metadata handling and improve data structure.

--- amplaRPackage/R/e_abc_xabc.R
+++ amplaRPackage/R/e_abc_xabc.R
@@ -4,21 +4,6 @@ e_abc_xabc <- function(f_caminho.arquivo_c) {
   )) %>%
     set_names("_1", "_2", "_3", "_4", "_5", "_6") %>%
     mutate(across(everything(), ~ str_squish(as.character(.))))
-
-  # Dados
-  indice.dados.comeco_i <- xabc.original_t %>%
-    pull(1) %>%
-    str_squish() %>%
-    str_which("^(?i)data\\s?do\\s?d[eé]b") %>%
-    first() + 1
-  indice.dados.fim_i <- xabc.original_t %>%
-    pull(ncol(xabc.original_t)) %>%
-    str_squish() %>%
-    str_which("(?:R\\$)?\\s?-?\\s?(?:\\d{1,3}(\\.\\d{3})*)?(\\,\\d{2})") %>%
-    last()
-  xabc.dados_t <- xabc.original_t %>%
-    slice(indice.dados.comeco_i:indice.dados.fim_i) %>%
-    set_names(c("data", "documento", "historico", "operacao", "valor", "saldo"))
   # Metadados
   cnpj_c <- xabc.original_t %>%
     dplyr::filter(str_detect(`_1`, "^(?i)cnpj")) %>%
@@ -52,15 +37,37 @@ e_abc_xabc <- function(f_caminho.arquivo_c) {
     pull(`_1`) %>%
     str_extract("\\d{2}/\\d{2}/\\d{4}$") %>%
     dmy()
-  xabc.metadados_t <- tibble::tibble(
-    cnpj = cnpj_c,
-    cliente = cliente_c,
-    data.consulta = data.consulta_dhms,
-    banco = banco_c,
-    agencia = agencia_c,
-    conta = conta_c,
-    periodo.inicio = periodo.inicio_d,
-    periodo.fim = periodo.fim_d
-  )
+  # Dados
+  indice.dados.comeco_i <- xabc.original_t %>%
+    pull(1) %>%
+    str_squish() %>%
+    str_which("^(?i)data\\s?do\\s?d[eé]b") %>%
+    first() + 1
+  indice.dados.fim_i <- xabc.original_t %>%
+    pull(ncol(xabc.original_t)) %>%
+    str_squish() %>%
+    str_which("(?:R\\$)?\\s?-?\\s?(?:\\d{1,3}(\\.\\d{3})*)?(\\,\\d{2})") %>%
+    last()
+  xabc_t <- xabc.original_t %>%
+    slice(indice.dados.comeco_i:indice.dados.fim_i) %>%
+    set_names(c(
+      "data", "documento", "descricao", "operacao", "valor", "saldo"
+    )) %>%
+    mutate(
+      agencia = agencia_c,
+      arquivo = f_caminho.arquivo_c,
+      banco = banco_c,
+      empresa = cliente_c,
+      cnpj = cnpj_c,
+      conta = conta_c,
+      data.consulta = data.consulta_dhms,
+      periodo.fim = periodo.fim_d,
+      periodo.inicio = periodo.inicio_d
+    ) %>%
+    select(
+      data, valor, saldo, descricao, empresa, cnpj, agencia, conta,
+      periodo.inicio, periodo.fim, data.consulta, arquivo, banco, documento,
+      operacao
+    )
   return(xabc_t)
 }
