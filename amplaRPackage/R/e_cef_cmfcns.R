1aeea3ef3325c47da1e4512fa407d8752f30dbad
vitortaira <vitortaira@gmail.com>
2025-09-26 17:29:22 -0300

    fix: Update regex for PDF file detection in e_cef_cmfcns function to be case-insensitive

--- amplaRPackage/R/e_cef_cmfcns.R
+++ amplaRPackage/R/e_cef_cmfcns.R
@@ -35,7 +35,7 @@ e_cef_cmfcns <- function(f_caminho.pasta.ciweb_c = caminhos_pastas("ciweb")) {
   # Consolida os dados dos relatórios CMF_CN na pasta "Relatorios - CIWEB"
   caminhos.cmfcn_c <-
     dir_ls(f_caminho.pasta.ciweb_c, recurse = TRUE, type = "file") %>%
-    keep(~ str_detect(.x, "MOV_FINANC_CN.pdf"))
+    keep(~ str_detect(.x, "(?i)mov_financ_cn.pdf"))
   # Identifica o arquivo mais recente de cada empreendimento
   contratos.empreendimentos.12.primeiros_c <-
     caminhos.cmfcn_c %>%
