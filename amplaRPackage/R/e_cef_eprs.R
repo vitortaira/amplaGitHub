bb7d39d1999fb8b6ef4cecc074451957a563a1a4
vitortaira <vitortaira@gmail.com>
2026-01-07 14:10:43 -0300

    feat: Refactor e_cef_eprs function to simplify parameters and enhance data extraction; remove deprecated test code feat: Replace outdated Informakon receipts file with updated version for improved data accuracy feat: Update Template-Fechamento.xlsx to reflect recent changes in data structure and formatting

--- amplaRPackage/R/e_cef_eprs.R
+++ amplaRPackage/R/e_cef_eprs.R
@@ -4,9 +4,6 @@
 #' A função **e_cef_eprs()** extrai e consolida os dados dos relatórios EPR da CEF
 #' que estão na pasta "Relatorios - CIWEB".
 #'
-#' @param f_caminho.pasta.ciweb_c Caminho para a pasta "Relatorios - CIWEB".
-#'   Padrão: \code{file.path(dirname(dirname(here())), "Relatórios - Documentos", "Relatorios - CIWEB")}.
-#'
 #' @details
 #' O processo busca todos os arquivos EPR com extensão \code{.pdf}, realiza a extração
 #' dos dados e consolida em um único \code{tibble}.
@@ -15,11 +12,7 @@
 #'
 #' @examples
 #' \dontrun{
-#' # Usando o caminho padrão
 #' e_cef_eprs()
-#'
-#' # Fornecendo um caminho específico:
-#' e_cef_eprs("C:/caminho/personalizado/Relatorios - CIWEB")
 #' }
 #'
 #' @importFrom fs dir_ls
@@ -29,55 +22,40 @@
 #' @importFrom tibble as_tibble
 #' @importFrom magrittr %>%
 #' @export
-e_cef_eprs <-
-  function(f_caminho.pasta.ciweb_c =
-             caminhos_pastas("ciweb")) {
-    # Consolida os dados dos relatórios EPR da CEF na pasta "Relatorios - CIWEB"
-    caminhos.epr_c <-
-      dir_ls(f_caminho.pasta.ciweb_c, recurse = TRUE, type = "file") %>%
-      keep(~ str_ends(.x, "(?i)contratos_empreend\\.pdf"))
-    # Identifica o arquivo mais recente de cada empreendimento
-    caminhos.epr.recentes_c <- tibble::tibble(caminho = caminhos.epr_c) %>%
-      mutate(
-        contrato = stringr::str_extract(caminho, "\\d{12}"),
-        data_arquivo = lubridate::ymd(stringr::str_extract(fs::path_file(caminho), "^\\d{8}"))
-      ) %>%
-      # Remove arquivos onde o contrato ou a data não puderam ser extraídos
-      dplyr::filter(!is.na(contrato) & !is.na(data_arquivo)) %>%
-      # Para cada contrato, encontra o arquivo mais recente
-      dplyr::group_by(contrato) %>%
-      dplyr::slice_max(order_by = data_arquivo, n = 1, with_ties = FALSE) %>%
-      dplyr::ungroup() %>%
-      dplyr::pull(caminho)
-    eprs_l <- list()
-    eprs_t <- data.frame()
-    for (
-      i_caminho.cef.epr_c in caminhos.epr.recentes_c
-    ) {
-      eprs_l[[i_caminho.cef.epr_c]] <-
-        e_cef_epr(i_caminho.cef.epr_c)
-      eprs_t <-
-        bind_rows(eprs_t, eprs_l[[i_caminho.cef.epr_c]])
-    }
-    eprs_t %<>% distinct(across(-arquivo), .keep_all = TRUE) %>%
-      as_tibble() %>%
-      mutate(
-        arquivo.tabela.tipo = "epr",
-        arquivo.tipo = "epr",
-        arquivo.fonte = "cef"
-      )
-    return(eprs_t)
+e_cef_eprs <- function() {
+  # Consolida os dados dos relatórios EPR da CEF na pasta "Relatorios - CIWEB"
+  caminhos.epr_c <-
+    dir_ls(caminhos_pastas("ciweb"), recurse = TRUE, type = "file") %>%
+    keep(~ str_ends(.x, "(?i)contratos_empreen(d)?\\.pdf"))
+  # Identifica o arquivo mais recente de cada empreendimento
+  caminhos.epr.recentes_c <- tibble::tibble(caminho = caminhos.epr_c) %>%
+    mutate(
+      contrato = stringr::str_extract(caminho, "\\d{12}"),
+      data_arquivo = lubridate::ymd(stringr::str_extract(fs::path_file(caminho), "^\\d{8}"))
+    ) %>%
+    # Remove arquivos onde o contrato ou a data não puderam ser extraídos
+    dplyr::filter(!is.na(contrato) & !is.na(data_arquivo)) %>%
+    # Para cada contrato, encontra o arquivo mais recente
+    dplyr::group_by(contrato) %>%
+    dplyr::slice_max(order_by = data_arquivo, n = 1, with_ties = FALSE) %>%
+    dplyr::ungroup() %>%
+    dplyr::pull(caminho)
+  eprs_l <- list()
+  eprs_t <- data.frame()
+  for (
+    i_caminho.cef.epr_c in caminhos.epr.recentes_c
+  ) {
+    eprs_l[[i_caminho.cef.epr_c]] <-
+      e_cef_epr(i_caminho.cef.epr_c)
+    eprs_t <-
+      bind_rows(eprs_t, eprs_l[[i_caminho.cef.epr_c]])
   }
-
-# Teste -------------------------------------------------------------------
-
-# f_caminho.pasta.ciweb_c <-
-#  here::here("..", "..", "Relatórios - Documentos", "Relatorios - CIWEB",
-#    "1. UP Vila Sonia", "11.03.25", "EPR",
-#    "20250311_123902_696_PP_177770014920_CONTRATOS_EMPREEND.pdf"
-#  )
-#  here::here("..", "..", "Relatórios - Documentos", "Relatorios - Extratos",
-#    "Matriz - Prudencia", "Fevereiro 2025", "EXTRATO 2429 - FEVEREIRO.pdf"
-#  )
-# eprs_t <- e_cef_eprs()
-# shell.exec(f_caminho.pasta.ciweb_c)
+  eprs_t %<>% distinct(across(-arquivo), .keep_all = TRUE) %>%
+    as_tibble() %>%
+    mutate(
+      arquivo.tabela.tipo = "epr",
+      arquivo.tipo = "epr",
+      arquivo.fonte = "cef"
+    )
+  return(eprs_t)
+}
